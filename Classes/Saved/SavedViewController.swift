2ad0ea0a2bc30651a810bef31eea917e3a38bfe6
Roger Prats <regorstarp@gmail.com>
2018-12-03 17:01:44 +0100

    Test

---
+++ Classes/Saved/SavedViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CompletedViewController.swift
+//  Escape Room Finder
+//
+//  Created by Roger Prats on 21/11/2018.
+//  Copyright © 2018 Roger Prats. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseUI
+
+class SavedViewController: UIViewController {
+
+    private let authUI = FUIAuth.defaultAuthUI()!
+    private var isUserLoggedIn : Bool {
+        get {
+            return Auth.auth().currentUser == nil ? false : true
+        }
+    }
+    private let userNotLoggedViewController = UserNotLoggedViewController(imageName: "bookmark-logo")
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if !isUserLoggedIn && !children.contains(userNotLoggedViewController){
+            add(userNotLoggedViewController)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Saved"
+        view.backgroundColor = .white
+        
+        if !isUserLoggedIn && !children.contains(userNotLoggedViewController){
+            add(userNotLoggedViewController)
+        }
+    }
+}
